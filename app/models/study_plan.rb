0b9929cea54533cc1c4d505b0b1c29c5f6323bbf
Rohan Khanna <rk868@njit.edu>
2025-04-19 17:36:35 -0400

    feat(models): introduce StudyPlanQuestion and UserActivity models

--- app/models/study_plan.rb
+++ app/models/study_plan.rb
@@ -1,3 +1,7 @@
 class StudyPlan < ApplicationRecord
   belongs_to :user
+  has_many :study_plan_companies, dependent: :destroy
+  has_many :companies, through: :study_plan_companies
+  has_many :study_plan_questions, dependent: :destroy
+  has_many :questions, through: :study_plan_questions
 end
