286cf2596e459c70a32510eda3a68de5707551c5
Rohan Khanna <rk868@njit.edu>
2025-04-19 17:36:35 -0400

    feat(import): dynamically seed Company list, support FAANG_ONLY flag

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,25 @@
+# db/seeds.rb
 require 'net/http'
 require 'json'
 require 'uri'
 
-puts "Fetching company list from GitHub…"
-uri   = URI("https://api.github.com/repos/liquidslr/leetcode-company-wise-problems/contents")
-res   = Net::HTTP.get_response(uri)
-items = JSON.parse(res.body)
+# If FAANG_ONLY is set, seed only FAANG companies for quick bootstrap
+if ENV['FAANG_ONLY'] == 'true'
+  puts "Seeding FAANG companies only..."
+  %w[Google Amazon Apple Netflix Meta].each do |name|
+    Company.find_or_create_by!(name: name)
+  end
+else
+  puts "Fetching full company list from GitHub…"
+  uri   = URI("https://api.github.com/repos/liquidslr/leetcode-company-wise-problems/contents")
+  res   = Net::HTTP.get_response(uri)
+  items = JSON.parse(res.body)
 
-dirs = items.select { |item| item["type"] == "dir" }.map { |item| item["name"] }
+  dirs = items.select { |item| item["type"] == "dir" }.map { |item| item["name"] }
 
-dirs.each do |company_name|
-  Company.find_or_create_by!(name: company_name)
+  dirs.each do |company_name|
+    Company.find_or_create_by!(name: company_name)
+  end
 end
 
 puts "Seeded #{Company.count} companies."
--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -1,7 +1,7 @@
 namespace :refresh do
     desc "Refresh LeetCode questions from GitHub CSVs"
     task questions: :environment do
-      GithubCsvImporter.refresh!
+      GithubCsvImporter.refresh_all!
       puts "Imported all questions at #{Time.current}"
     end
   end
