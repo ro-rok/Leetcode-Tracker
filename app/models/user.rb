9272789c247ee15a5f79a829e12d50498055ecd3
Rohan Khanna <rk868@njit.edu>
2025-04-19 17:36:35 -0400

    feat(import): dynamically seed Company list, support FAANG_ONLY flag

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :study_plans, dependent: :destroy
+  has_many :user_activities, dependent: :destroy
 end
