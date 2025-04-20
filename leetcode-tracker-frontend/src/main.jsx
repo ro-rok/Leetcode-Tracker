a38614bc82c88cb77e27331e37635952c0519bb2
Rohan Khanna <rk868@njit.edu>
2025-04-19 23:11:28 -0400

    feat(frontend): set up React Router with Login, Signup and main App

--- leetcode-tracker-frontend/src/main.jsx
+++ leetcode-tracker-frontend/src/main.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
-import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+import App from './App'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+)
