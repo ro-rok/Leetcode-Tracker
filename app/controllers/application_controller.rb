319d8165f05b96b37a2d4d121c7e9b2a7a217637
Rohan Khanna <rk868@njit.edu>
2025-04-22 13:07:13 -0400

    chore(controller): update ApplicationController with CORS support for frontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,21 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session, unless: -> { request.format.json? }
+
+  # ✅ Override to fix Devise's `flash=` call issue
+  def handle_unverified_request
+    self.request.session_options[:skip] = true
+    super
+  rescue NoMethodError
+    # ignore flash error in request object (API calls)
+    render json: { error: "Invalid authenticity token" }, status: :unauthorized
+  end
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  # Devise strong params for sign_up & sign_in
   def configure_permitted_parameters
-    # For sign up
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation])
-    # For sign in
     devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password])
   end
 end
