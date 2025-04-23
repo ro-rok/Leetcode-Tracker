1ad60df84b5ed90a44f2ee0547153b137d0ad269
Rohan Khanna <rk868@njit.edu>
2025-04-23 13:10:49 -0400

    fix(controller): reset_progress now supports stateless user via user_id param

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,29 @@
-# app/controllers/users_controller.rb
 class UsersController < ApplicationController
-    before_action :authenticate_user!
-  
-    # POST /users/reset_progress.json
-    # If you pass `company_id` in the body, only that company's progress is reset.
-    def reset_progress
-      if params[:company_id].present?
-        # only delete solves for questions belonging to that company
-        current_user.user_questions
-                    .joins(:question)
-                    .where(questions: { company_id: params[:company_id] })
-                    .destroy_all
-      else
-        # delete all solves
-        current_user.user_questions.destroy_all
-      end
-  
-      head :no_content
+  skip_before_action :verify_authenticity_token, only: [:reset_progress]
+  skip_before_action :authenticate_user!, raise: false
+  before_action :set_optional_user
+
+  # POST /users/reset_progress.json
+  def reset_progress
+    unless @current_user
+      render json: { error: "Missing or invalid user_id" }, status: :unauthorized and return
     end
+
+    if params[:company_id].present?
+      @current_user.user_questions
+                  .joins(:question)
+                  .where(questions: { company_id: params[:company_id] })
+                  .destroy_all
+    else
+      @current_user.user_questions.destroy_all
+    end
+
+    head :no_content
   end
-  
+
+  private
+
+  def set_optional_user
+    @current_user = User.find_by(id: params[:user_id])
+  end
+end
