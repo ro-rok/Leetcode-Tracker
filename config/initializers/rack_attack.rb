681512283e39c7cca84aa12431af4eea25cbb7b1
Rohan Khanna <rk868@njit.edu>
2025-05-07 22:27:40 -0400

    chore(rack): remove deprecated code

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,16 +1,15 @@
 class Rack::Attack
-    # throttle POST /questions/:id/chat to 5 requests per minute per user/IP
-    throttle('ai_chat/user', limit: 5, period: 1.minute) do |req|
-      if req.path.match?(/^\/questions\/\d+\/chat/) && req.post?
-        req.env['warden'].user&.id || req.ip
-      end
-    end
-  
-    self.throttled_response = lambda do |env|
-      [ 429, { 'Content-Type' => 'application/json' },
-        [{ error: "Rate limit exceeded. Try again later." }.to_json] ]
+  throttle('ai_chat/user', limit: 5, period: 1.minute) do |req|
+    if req.path.match?(/^\/questions\/\d+\/chat/) && req.post?
+      req.env['warden'].user&.id || req.ip
     end
   end
-  
-  Rails.application.config.middleware.use Rack::Attack
-  
+
+  # Use throttled_responder instead of throttled_response
+  self.throttled_responder = lambda do |env|
+    [ 429, { 'Content-Type' => 'application/json' },
+      [{ error: "Rate limit exceeded. Try again later." }.to_json] ]
+  end
+end
+
+Rails.application.config.middleware.use Rack::Attack
